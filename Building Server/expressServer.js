643e358974b66b215bc3465fd7a6d645950db164
Rizwanjamal <Rizwan_hassanfatah@hotmail.com>
2019-06-04 16:19:07 +0500

    Created GET Routes

--- Building Server/expressServer.js
+++ Building Server/expressServer.js
@@ -2,11 +2,24 @@ const express = require('express');
 
 const app = express();
 
+let books = [{
+    id: 1,
+    name: 'A Smarter Way To Learn Javascript'
+}, {
+    id: 2,
+    name: 'A Smarter Way To Learn Python'
+}];
+
 app.get('/', (req, res) => {
     res.send('Hello World !');
 });
 
-const port = 3000;
+app.get('/books', (req, res) => {
+    console.log('query :', req.query);
+    res.send(books);
+});
+
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
     console.log(`Server Running At Port ${port}`)
