12271bfc7d5c5b8b056cefa368ee10d101d051d9
Rizwanjamal <Rizwan_hassanfatah@hotmail.com>
2019-06-04 16:17:23 +0500

    Build Server using Express Framework

---
+++ Building Server/expressServer.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello World !');
+});
+
+const port = 3000;
+
+app.listen(port, () => {
+    console.log(`Server Running At Port ${port}`)
+});
