b38107ab951fbfefc639a31d36f4ef6b68f3721b
Rizwanjamal <Rizwan_hassanfatah@hotmail.com>
2019-06-23 10:33:18 +0500

    Added post & put routes with validations

--- Building Server/expressServer.js
+++ Building Server/expressServer.js
@@ -1,6 +1,20 @@
 const express = require('express');
-
+const joi = require('@hapi/joi');
+const logger = require('./logger');
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static('public'));
+
+//Custom Middlewares
+
+app.use(logger);
+
+app.use((req, res, next) => {
+    console.log('Second Middleware');
+    next();
+});
 
 let books = [{
     id: 1,
@@ -11,7 +25,8 @@ let books = [{
 }];
 
 app.get('/', (req, res) => {
-    res.send('Hello World !');
+    // res.send('Hello World !');
+    res.render('main');
 });
 
 // Fetch Request Query Parameters With req.query
@@ -28,6 +43,39 @@ app.get('/books/:id', (req, res) => {
     const book = books.find( b => b.id === parseInt(req.params.id));
     res.send(book);
 });
+
+// Add New Book With Validations
+app.post('/books', (req, res) => {
+    console.log('body :', req.body);
+    const { error } = validatePayload(req.body);
+    const book = {
+        id: books.length + 1,
+        name: req.body.name 
+    };
+    if (error) return res.status(400).send(error.details[0].message);
+    books.push(book);
+    res.send(book);
+})
+
+
+// Add Book With Validations
+app.put('/books/:id', (req, res) => {
+    const book = books.find( book => book.id === parseInt(req.params.id));
+    if (!book) return res.status(400).send('Book Not Found ! Please provide a valid ID')
+    const { error } = validatePayload(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+    
+    book.name = req.body.name;
+    res.send(book)
+}) 
+
+// Validate Payload Functionality
+const validatePayload = (data) => {
+    const schema = {
+        name: joi.string().min(3).max(30).required()
+    }
+    return joi.validate(data, schema);
+};
 
 const port = process.env.PORT || 3000;
 
